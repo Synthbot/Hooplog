ac42382adae2241d5048c2d9a526f4e2955e4515
Matthew Beebe <mattbb24@gmail.com>
2018-03-21 12:01:38 -0400

    adds the Spec files.

---
+++ src/test/groovy/com/manifest/CorpSpec.groovy
@@ -0,0 +1,18 @@
+package com.manifest
+
+import grails.testing.gorm.DomainUnitTest
+import spock.lang.Specification
+
+class CorpSpec extends Specification implements DomainUnitTest<Corp> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
